dce7b3bfa43cfc67ffe998f104bd98411f0f79a9
Alan <alan.wei@live.com>
2019-06-02 21:30:25 +0800

    feat: update nuget spec

--- nuget/pack.sh
+++ nuget/pack.sh
@@ -1,2 +1,2 @@
 rm -rf ./publish
-dotnet pack ../Argument.Net/Argument.Net.csproj --output ../nuget/publish --include-symbols --include-source --configuration Release #-p:PackageVersion=0.0.1 
+dotnet pack ../Argument.Net/Argument.Net.csproj --output ../nuget/publish --include-symbols --include-source --configuration Release 
